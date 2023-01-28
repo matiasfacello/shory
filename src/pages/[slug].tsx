c90ccd03089f7b5cf3f6c0bc8ad7b755e85e542c
matiasfacello <matiasfacello@gmail.com>
2023-01-28 12:04:20 -0300

    updates logic of redirect, still needs work

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,31 +1,57 @@
-import { useRouter } from "next/router";
-// import { useEffect } from "react";
+//import type { InferGetStaticPropsType } from "next"; { link }: InferGetStaticPropsType<typeof getStaticProps>
+import Image from "next/image";
+import { env } from "../env/server.mjs";
 
-const Link = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+interface Link {
+  url: string;
+  slug: string;
+  utm_campaign: string;
+  utm_content: string;
+  utm_medium: string;
+  utm_source: string;
+  utm_term: string;
+}
 
-  const link = async () => {
-    const link = await fetch(`/api/get-url/${slug}`);
-
-    if (link.status === 404) {
-      router.replace("/");
-    }
-
-    const url = await link.json();
-
-    if (url?.url) {
-      router.replace(url.url);
-    }
+interface Context {
+  params: {
+    slug: string;
   };
+}
 
-  link();
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { slug: "*" } }],
+    fallback: true,
+  };
+}
 
+export const getStaticProps = async (context: Context) => {
+  const dir = env.NEXTAUTH_URL + "/api/link/" + context.params.slug;
+  const res = await fetch(dir);
+  const link = await res.json();
+  const linkSource = link.utm_source ? "&utm_source=" + link.utm_source : "";
+  const linkCampaign = link.utm_campaign ? "&utm_campaign=" + link.utm_campaign : "";
+  const linkMedium = link.utm_medium ? "&utm_medium=" + link.utm_medium : "";
+  const linkTerm = link.utm_term ? "&utm_term=" + link.utm_term : "";
+  const linkContent = link.utm_content ? "&utm_content=" + link.utm_content : "";
+  const goTo = link.url + "/?" + linkSource + linkCampaign + linkMedium + linkTerm + linkContent;
+  return {
+    redirect: {
+      destination: goTo,
+      permanent: true,
+    },
+  };
+};
+
+function Link() {
   return (
     <>
-      <div className="h-full w-full bg-black"></div>
+      <div className="min-w-screen flex min-h-screen flex-col items-center justify-center bg-slate-900">
+        <Image src="/img/puff.svg" alt="Loading" width={50} height={50} />
+        <h2 className="m-8 block text-2xl text-white">Redirecting</h2>
+      </div>
     </>
   );
-};
+}
 
 export default Link;
