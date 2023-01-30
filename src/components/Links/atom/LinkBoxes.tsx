d455fc3a7d4f6e557cbca8542052a5455f01ffe2
matiasfacello <matiasfacello@gmail.com>
2023-01-30 18:40:37 -0300

    adds close button to link boxes

--- src/components/Links/atom/LinkBoxes.tsx
+++ src/components/Links/atom/LinkBoxes.tsx
@@ -1,9 +1,12 @@
+import { Icon } from "@iconify-icon/react";
 import React from "react";
 
 interface FieldProps {
   children: React.ReactNode;
+  closeButton?: () => void;
   title: string;
   classBox?: string;
+  classClose?: string;
   classTitle?: string;
   classChildren?: string;
 }
@@ -11,6 +14,7 @@ interface FieldProps {
 const LinkBoxes = (props: FieldProps) => {
   return (
     <div className={"flex w-full flex-col justify-center gap-4 py-4 " + props.classBox}>
+      {props.closeButton && <Icon icon="material-symbols:close" className={"-mb-6 block cursor-pointer self-end px-6 text-4xl " + props.classClose} onClick={props.closeButton} />}
       <p className={"p-1 text-center text-2xl font-bold " + props.classTitle}>{props.title}</p>
       <div className={"flex flex-wrap justify-center gap-6 " + props.classChildren}>{props.children}</div>
     </div>
