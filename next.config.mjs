65be8bd7c52df841f93a991aeca80d9a11ea3ddd
matiasfacello <matiasfacello@gmail.com>
2023-02-18 16:01:52 -0300

    config: updates to use discord images

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+    ],
+  },
 };
 export default config;
