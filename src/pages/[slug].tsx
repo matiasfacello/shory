19b9b2a3e5e6a9d079fed7928dd811ea7f1d49b9
matiasfacello <matiasfacello@gmail.com>
2023-02-19 22:00:42 -0300

    feat: changes redirect method to next router push

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,20 +1,21 @@
-//import type { InferGetStaticPropsType } from "next"; { link }: InferGetStaticPropsType<typeof getStaticProps>
-import Image from "next/image";
+import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { env } from "../env/server.mjs";
 
-interface Link {
-  url: string;
-  slug: string;
-  utm_campaign: string;
-  utm_content: string;
-  utm_medium: string;
-  utm_source: string;
-  utm_term: string;
-}
+import Head from "next/head.js";
+import Image from "next/image";
 
 interface Context {
   params: {
     slug: string;
+  };
+}
+
+interface DataProps {
+  data: {
+    status: string;
+    url: string;
   };
 }
 
@@ -30,11 +31,14 @@ export const getStaticProps = async (context: Context) => {
   const res = await fetch(dir);
   const link = await res.json();
 
-  if (link.status === "error") {
+  if (link.status == "error") {
+    const data = {
+      status: "error",
+      url: "/",
+    };
     return {
-      redirect: {
-        destination: "/",
-        permanent: true,
+      props: {
+        data,
       },
     };
   }
@@ -45,23 +49,42 @@ export const getStaticProps = async (context: Context) => {
   const linkTerm = link.utm_term ? "&utm_term=" + link.utm_term : "";
   const linkContent = link.utm_content ? "&utm_content=" + link.utm_content : "";
   const goTo = link.url + "/?" + linkSource + linkCampaign + linkMedium + linkTerm + linkContent;
+
+  const data = {
+    status: "success",
+    url: goTo,
+  };
   return {
-    redirect: {
-      destination: goTo,
-      permanent: true,
+    props: {
+      data,
     },
   };
 };
 
-function Link() {
+const Link: NextPage<DataProps> = ({ data }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (data?.status === "success") {
+      router.push(data.url);
+    } else if (data?.status === "error") {
+      router.push(data.url);
+    }
+  }, [router, data]);
+
   return (
     <>
+      <Head>
+        <title>Redirecting - Shory</title>
+        <meta name="description" content="Shory - Create your short links and track them!" />
+        <link rel="icon" href="/img/logo.svg" />
+      </Head>
       <div className="min-w-screen flex min-h-screen flex-col items-center justify-center bg-slate-900">
         <Image src="/img/puff.svg" alt="Loading" width={50} height={50} />
         <h2 className="m-8 block text-2xl text-white">Redirecting</h2>
       </div>
     </>
   );
-}
+};
 
 export default Link;
