dfbcd4ae6f52ec2e6eab4a7b1df0deafc01b09f9
matiasfacello <matiasfacello@gmail.com>
2023-02-20 23:12:46 -0300

    fix: white box left from last update in component

--- src/components/Links/ListForUser.tsx
+++ src/components/Links/ListForUser.tsx
@@ -21,10 +21,10 @@ const LinkList: React.FC = () => {
       {isLoadingMutation ? (
         <h2 className="py-8 text-center text-4xl font-bold">Loading</h2>
       ) : linkList && linkList?.length > 0 ? (
-        <div className="mx-auto my-16 grid max-w-7xl grid-cols-1 items-center justify-center gap-6 rounded bg-white p-4 text-black">
+        <>
           <h2 className="py-8 text-center text-4xl font-bold">Your links</h2>
           <div className="flex flex-col transition">{linkList ? linkList?.map((link) => <Item {...link} key={link.slug} />) : <p className="text-xl">No links yet.</p>}</div>
-        </div>
+        </>
       ) : (
         <h2 className="py-8 text-center text-4xl font-bold">No links yet.</h2>
       )}
