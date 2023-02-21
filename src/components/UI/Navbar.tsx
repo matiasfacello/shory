ecaf3597549af5abc2ddf4e5d814ab3fc504752d
matiasfacello <matiasfacello@gmail.com>
2023-02-20 23:12:18 -0300

    feat: new logo component for navbar

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
-import logo from "~/../public/img/logo.svg";
-import Link from "next/link";
-
 import AuthBar from "~/components/User/AuthBar";
 import NavbarLinks from "~/components/UI/widgets/NavbarLinks";
+import Logo from "~/components/UI/widgets/Logo";
 
 const Navbar: React.FC = () => {
   return (
     <nav className=" flex h-24 w-full flex-wrap items-center justify-between gap-4 bg-slate-900 px-6 lg:px-20">
       <div className="container mx-auto flex items-center justify-between gap-6">
-        <Link href="/" className="flex w-1/6 items-center justify-start gap-2 text-center text-2xl font-extrabold tracking-tight text-white lg:gap-4 lg:text-4xl">
-          <Image src={logo} alt="Shory Logo" className="w-8 lg:w-10" />
-          Shory
-        </Link>
+        <Logo classBox="w-1/6 justify-start" />
         <NavbarLinks />
         <AuthBar />
       </div>
---
+++ src/components/UI/widgets/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import logo from "~/../public/img/logo.svg";
+import Link from "next/link";
+
+interface FieldProps {
+  classBox?: string;
+  classImage?: string;
+  classText?: string;
+}
+
+const Logo = (props: FieldProps) => {
+  return (
+    <Link href="/" className={"flex items-center gap-2 text-2xl font-extrabold tracking-tight text-white lg:gap-4 lg:text-4xl " + props.classBox}>
+      <Image src={logo} alt="Shory Logo" className={"w-8 lg:w-10 " + props.classImage} />
+      <span className={props.classText}>Shory</span>
+    </Link>
+  );
+};
+
+export default Logo;
