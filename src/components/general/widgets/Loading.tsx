71370c27c1827c2d5ca7e9b5a48e23c083adf504
matiasfacello <matiasfacello@gmail.com>
2023-04-10 18:25:49 -0300

    feat: new puff component

---
+++ src/components/general/widgets/Loading.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Puff from "~/assets/puff.svg";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="min-w-screen -mt-32 flex min-h-screen flex-col items-center justify-center">
+        <Image src={Puff} alt="Loading" width={50} height={50} />
+        <h2 className="m-8 block text-2xl text-white">Loading</h2>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
