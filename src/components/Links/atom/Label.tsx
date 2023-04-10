287e48f49f33dd3f34797be643a0d5fa966fdf01
matiasfacello <matiasfacello@gmail.com>
2023-04-10 18:27:54 -0300

    feat: now with required asterisk

--- src/components/Links/atom/Label.tsx
+++ src/components/Links/atom/Label.tsx
@@ -2,12 +2,18 @@ interface FieldProps {
   for: string;
   name: string;
   className?: string;
+  required?: boolean;
 }
 
 const Label = (props: FieldProps) => {
   return (
     <label htmlFor={props.for} className={"font-bold " + props.className}>
       {props.name}
+      {props.required && (
+        <span className="font-bold italic text-red-300" title="Required">
+          *
+        </span>
+      )}
     </label>
   );
 };
